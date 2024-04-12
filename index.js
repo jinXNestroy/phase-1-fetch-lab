505b744b4f2effbe893c66a65443249d108e1bb7
DanielJoel <danpersie53@gmail.com>
2024-04-12 12:03:27 +0300

    Fetch lab

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books').then(function (response) { return response.json() }).then(renderBooks)
+
 }
 
 function renderBooks(books) {
@@ -12,6 +13,6 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
